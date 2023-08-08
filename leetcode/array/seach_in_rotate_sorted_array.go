553283ddb12b1aae96ac42e02adc08259f7389a0
Elias Andualem <eliasandualem8@gmail.com>
2023-08-08 23:52:24 +0300

    leetcode(33): Search in Rotated Sorted Array

---
+++ leetcode/array/seach_in_rotate_sorted_array.go
@@ -0,0 +1,29 @@
+package array
+
+// 33. Search in Rotated Sorted Array
+//
+// https://leetcode.com/problems/search-in-rotated-sorted-array
+// O(log n) time | O(1) space
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+
+	}
+	return -1
+}
