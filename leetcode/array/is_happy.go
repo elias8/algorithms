b67d40f43ca2cbd6d739a3d01ca9f0928662bc85
Elias Andualem <eliasandualem8@gmail.com>
2023-09-12 01:12:33 +0300

    leetcode(202): Happy Number

---
+++ leetcode/array/is_happy.go
@@ -0,0 +1,29 @@
+package array
+
+import "math"
+
+func main() {
+	println(isHappy(2))
+}
+
+// 202. Happy Number
+// https://leetcode.com/problems/happy-number
+//
+// O(n) time | O(1) space
+func isHappy(n int) bool {
+	sum := 0
+	num := n
+	for num > 0 {
+		sum += int(math.Pow(float64(num%10), 2))
+		num = num / 10
+		if num < 10 {
+			sum += int(math.Pow(float64(num), 2))
+			num = sum
+			if sum == 1 {
+				return true
+			}
+			sum = 0
+		}
+	}
+	return false
+}
