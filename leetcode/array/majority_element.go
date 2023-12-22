1196fdfa0277e5a74a4a9d5f86e3407230458c1f
Elias Andualem <eliasandualem8@gmail.com>
2023-12-23 00:18:15 +0300

    leetcode(169): [improve] Majority Element

--- leetcode/array/majority_element.go
+++ leetcode/array/majority_element.go
@@ -3,16 +3,18 @@ package array
 // 169. Majority Element
 // https://leetcode.com/problems/majority-element
 //
-// O(n) time | O(n) time
+// O(n) time | O(1) time
 func majorityElement(nums []int) int {
-	elements := map[int]int{}
-	majorityNum, majorityElement := 0, 0
-	for i := 0; i < len(nums); i++ {
-		elements[nums[i]]++
-		if elements[nums[i]] > len(nums)/2 && elements[nums[i]] > majorityElement {
-			majorityElement = elements[nums[i]]
-			majorityNum = nums[i]
+	var major, frequency int
+	for _, num := range nums {
+		if frequency == 0 {
+			major = num
+		}
+		if num == major {
+			frequency++
+		} else {
+			frequency--
 		}
 	}
-	return majorityNum
+	return major
 }
